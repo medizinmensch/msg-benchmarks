def0d6a239ababc1dc62f54562c19f8d846a3401
Youri Seichter <yseichter@gmail.com>
2021-03-09 09:38:36 +0100

    Add xpubxsub modell
    python working, I don't think C is working right now

---
+++ czmq/xpubxsub/py/rrclient.py
@@ -0,0 +1,20 @@
+#
+#   Request-reply client in Python
+#   Connects REQ socket to tcp://localhost:5559
+#   Sends "Hello" to server, expects "World" back
+#
+import sys
+import zmq
+
+client = sys.argv[1]
+print(f"Starting Client No. {client}")
+#  Prepare our context and sockets
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5559")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(1,11):
+    socket.send_string(f"{client} - Hello")
+    message = socket.recv()
+    print("Received reply %s [%s]" % (request, message))
---
+++ czmq/xpubxsub/py/runner.sh
@@ -0,0 +1,10 @@
+python3 rrclient.py 1 &
+python3 rrclient.py 2 &
+python3 rrclient.py 3 &
+python3 rrclient.py 4 &
+python3 rrclient.py 5 &
+python3 rrclient.py 6 &
+
+python3 rrbroker.py &
+
+python3 rrworker.py
