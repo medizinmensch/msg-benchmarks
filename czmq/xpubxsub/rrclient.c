2539060a005c3040a8c6bdea09b5a94763f9048f
Youri Seichter <yseichter@gmail.com>
2021-03-14 13:20:31 +0100

    switch to ints for client_id; refactor

--- czmq/xpubxsub/rrclient.c
+++ czmq/xpubxsub/rrclient.c
@@ -10,7 +10,7 @@
 
 #include "zhelpers.h"
 
-int _send_msgs(char *msg, int reps, char *conn, char *client)
+int _send_msgs(char *msg, int reps, char *conn, int client)
 {
     struct timespec start, end;
 
@@ -25,9 +25,9 @@ int _send_msgs(char *msg, int reps, char *conn, char *client)
     {
         // printf("Try sending [%s]\n", msg);
         s_send(requester, msg);
-        char *string = s_recv(requester);
-        // printf("Received reply %d [%s]\n", request_nbr, string);
-        free(string);
+        char *msg_recvd = s_recv(requester);
+        // printf("Received reply %d [%s]\n", request_nbr, msg_recvd);
+        free(msg_recvd);
     }
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
 
@@ -65,59 +65,48 @@ char *rand_string_alloc(size_t size)
     return s;
 }
 
-char *build_msg(size_t size, char *client_id)
+char *build_msg(size_t msg_size, int client_id)
 {
-    char *msg = rand_string_alloc(size);
-    size_t s = sizeof(client_id);
-    for (int i = 0; i < s - 2; i++)
+    char *msg = rand_string_alloc(msg_size);
+
+    int client_str_size = 9;
+    char client_str[client_str_size];
+
+    snprintf(client_str, client_str_size, "%007d", client_id); // save with leading zeros
+
+    for (int i = 0; i < client_str_size - 1; i++)
     {
-        msg[i] = client_id[i];
+        msg[i] = client_str[i];
     }
 
-    msg[s - 2] = ';';
+    msg[client_str_size] = ';';
 
     return msg;
 }
 
-// -----------------------------------------------------------
-
-// int signal_close(char *conn)
-// {
-//     printf("Connection closed\n");
-//     zsock_t *requester = zsock_new(ZMQ_REQ);
-//     zsock_connect(requester, conn);
-//     zstr_send(requester, "FINALMSG");
-//     char *str = zstr_recv(requester);
-//     zstr_free(&str);
-//     zsock_destroy(&requester);
-//     return 0;
-// }
-
-void bench_zmq(int repetitions, char *connection, char *client_id)
+void bench_zmq(int repetitions, char *connection, int client_id)
 {
-    printf("Repetitions, Message Size in characters, protocoll used, Elapsed time in us\n");
+    // printf("Repetitions, Message Size in characters, protocoll used, Elapsed time in us\n");
     char *msg;
-    for (int i = 1; i < 7; i++)
+    for (int i = 1; i < 8; i++)
     {
         double msg_size = pow(10, i);
-        printf("%d,%.0f,%s,", repetitions, msg_size, connection);
+        // printf("%d,%.0f,%s,", repetitions, msg_size, connection);
 
         msg = build_msg(msg_size + 1, client_id);
         _send_msgs(msg, repetitions, connection, client_id);
-        // infinity loop
-        // i--;
+        
+        // i--; // infinity loop
     }
 }
 
 int main(int argc, char *argv[])
 {
     char *conn = argv[1];
-    char *client_id = argv[2];
-    // printf("%s\n", conn);
-
-    bench_zmq(100000, conn, client_id);
-
-    // signal_close(conn);
+    int client_id = strtol(argv[2], NULL, 10);
+    
+    int reps = 100000;
+    bench_zmq(reps, conn, client_id);
 
     return 0;
 }
--- czmq/xpubxsub/rrworker.c
+++ czmq/xpubxsub/rrworker.c
@@ -1,66 +1,59 @@
 //  Hello World worker
-//  Connects REP socket to tcp://localhost:5560
-//  Expects "Hello" from client, replies with "World"
 
 #include "zhelpers.h"
 
+#include <inttypes.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <inttypes.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    char *conn = argv[1];
-
+    char *connection_string = argv[1];
     void *context = zmq_ctx_new();
+
+    int max_client_id_digits = 8;
 
     struct timespec start, end;
 
-    //  Socket to talk to clients
     void *responder = zmq_socket(context, ZMQ_REP);
-    zmq_connect(responder, conn);
+    zmq_connect(responder, connection_string);
+    int reps = 5000;
 
+    printf("\nThroughput in KiBi/second\n");
     while (1)
     {
         int bytes_recieved = 0;
         clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 
-        for (int i = 0; i < 5000; i++)
+
+        for (int i = 0; i < reps; i++)
         {
             //  Wait for next request from client
             char *string = s_recv(responder);
-            char client[7];
+            char client[max_client_id_digits + 1];
 
-            for (int c = 0; c < 6; c++)
+            for (int c = 0; c < max_client_id_digits; c++)
             {
                 client[c] = string[c];
             }
-            client[6] = '\0';
+            client[max_client_id_digits] = '\0';
 
             bytes_recieved = bytes_recieved + strlen(string);
+        
 
-            //  printf("Received request from client: [%s] of size [%zu]\n", client, strlen(string));
-
-            //  Do some 'work'
-
-            //  Send reply back to client
             s_send(responder, client);
             free(string);
-
-            // printf(".");
         }
 
+        
         clock_gettime(CLOCK_MONOTONIC_RAW, &end);
         uint64_t delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
-        printf("%" PRIu64, delta_us);
-        float kibips = ((float)bytes_recieved * 976.5625) / (float)delta_us;
+        // printf("%" PRIu64, delta_us); //724066
+        double kibips = ((float)bytes_recieved * 976.5625) / (float)delta_us;
 
-        // printf("Received [%zu] bytes in \r %" PRIu64 "\r", bytes_recieved, delta_ns);
+        printf("%.2f\n", kibips);
         bytes_recieved = 0;
-        printf("Current throughput: %.4f KiB/s \n", kibips);
-        // printf("%" PRIu64, delta_ns);
-        // printf("ns\r");
     }
     //  We never get here, but clean up anyhow
     zmq_close(responder);
